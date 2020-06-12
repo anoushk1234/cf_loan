8ada6dc766d1f002aa17b2751b4bf8e766fb65f0
anoushk1234 <32778608+anoushk1234@users.noreply.github.com>
2020-06-12 12:24:52 +0530

    Create installments.dart

---
+++ installments.dart
@@ -0,0 +1,13 @@
+class Installment {
+
+  int month;
+  double payment;
+  double principal;
+  double interest;
+  double totalrate ;
+  double balance;
+
+  Installment({this.month,this.payment,this.principal,this.interest,this.totalrate,this.balance});
+
+}
+
